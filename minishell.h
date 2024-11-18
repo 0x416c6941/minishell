fd8c18e8ebe9f3279c765a2e48981528466cd5d1
Hlib Lyshchuk <hlyshchu@c2r8s1.42prague.com>
2024-11-18 15:24:02 +0100

    New struct

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: hlyshchu <hlyshchu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:34:55 by hlyshchu          #+#    #+#             */
-/*   Updated: 2024/11/18 13:34:15 by hlyshchu         ###   ########.fr       */
+/*   Updated: 2024/11/18 15:22:01 by hlyshchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,18 +43,32 @@ typedef struct s_info
 	const size_t cmds_size;
 } t_info;
 
+typedef struct s_stdout_file
+{
+	char *output_file;
+	int append;
+	t_stdout_file *next;
+}	t_stdout_file;
+
 typedef struct s_command
 {
+	char *path_to_exec;
 	char **args;			// Array of arguments
 	size_t arg_count;		// Number of arguments
 	char *input_file;		// Input redirection file
-	char *output_file;		// Output redirection file
+	char **output_file;		// Output redirection filee
 	int append;				// Append mode (1 if `>>`, 0 otherwise)
 	struct s_command *next; // Pointer to the next command (for pipes)
 } t_command;
 
+typedef struct s_ret_check
+{
+	int status;	// -1 in case of an error
+	void *ret;	// Obmazatsya: ukazatel na cho ugodno
+}	t_ret_check;
+
 // Function prototypes
-t_command *parse_input(const char *input);
+t_ret_check parse_input(const char *input);
 void free_command(t_command *cmd);
 
 // Declare signal handling setup function
@@ -68,6 +82,41 @@ void setup_signals(void);
 // utils.c
 bool ft_isspace(const char c);
 bool check_unclosed_quotes(const char *str);
+
+
+
+
+
+typedef struct s_ret_check
+{
+	int status;	// -1 in case of an error
+	void *ret;	// Obmazatsya: ukazatel na cho ugodno
+}	t_ret_check;
+
+typedef struct s_stdin_file
+{
+	const char *stdin_file;		// If $is_heredoc isn't 0, this will be ending word for heredoc
+	unsigned short is_heredoc;	// 0 - not heredoc, everything else - heredoc
+	t_stdin_file *next;
+}	t_stdin_file;
+
+typedef struct s_stdout_file
+{
+	char *output_file;
+	int append;
+	t_stdout_file *next;
+}	t_stdout_file;
+
+typedef struct t_exec
+{
+
+	char *path_to_exec; //asagymba task
+	char **args;
+	t_stdin_file *stdin_file;
+	t_stdout_file *stdout_file;	// 0 - '>'
+	t_exec *next;
+}	t_exec;
+
 
 #endif /* MINISHELL_H */
 >>>>>>> fdc67c1 (Adding utils.c with check_unclosed_quotes and ft_isspace)
