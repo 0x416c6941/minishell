4d5398fdd14840a9dc1335d08d528740f2848e0a
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-11-28 20:33:42 +0100

    input_validation.h: Add a header guard

--- include/input_validation.h
+++ include/input_validation.h
@@ -6,9 +6,14 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 11:35:47 by root              #+#    #+#             */
-/*   Updated: 2024/11/28 11:54:11 by root             ###   ########.fr       */
+/*   Updated: 2024/11/28 20:33:10 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef INPUT_VALIDATION_H
+# define INPUT_VALIDATION_H
+
 int	input_issspace(const char *str);
 int	validate_input(const char *str);
+
+#endif /* INPUT_VALIDATION_H */
