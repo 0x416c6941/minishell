8b11f12c9bff915d261c25cc8a4a026a2b22d6e0
hlyshchu <gleb.lischuk@gmail.com>
2024-11-29 21:44:52 +0100

    Small fix in input_validation.h. Now it should compile for testing

--- include/input_validation.h
+++ include/input_validation.h
@@ -6,17 +6,14 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 11:35:47 by root              #+#    #+#             */
-/*   Updated: 2024/11/29 21:40:04 by root             ###   ########.fr       */
+/*   Updated: 2024/11/29 21:44:04 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INPUT_VALIDATION_H
 # define INPUT_VALIDATION_H
 
-# ifndef INPUT_VALIDATION_H
-#  define INPUT_VALIDATION_H
-
 int	input_issspace(const char *str);
 int	validate_input(const char *str);
 
-# endif /* INPUT_VALIDATION_H */ I
+#endif /* INPUT_VALIDATION_H */
