8c1c9cf8861148a7d33f61ae16b51bd03ca2ff78
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-11-30 00:24:40 +0100

    ft_getenv.c: Rename variable to better show what it holds

--- src/expander/ft_getenv.c
+++ src/expander/ft_getenv.c
@@ -6,28 +6,24 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:55:25 by root              #+#    #+#             */
-/*   Updated: 2024/11/29 22:09:15 by asagymba         ###   ########.fr       */
+/*   Updated: 2024/11/30 00:23:04 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <expander.h>
+#include <stddef.h>
+#include <stdlib.h>
 
-/**
- * @brief Retrieves the value of an environment variable or `$?` for exit code.
- *
- * @param name The variable name, `$?` returns the last exit code.
- * @return char* Dynamically allocated string of the variable's value, or NULL.
- */
 char	*ft_getenv(const char *name)
 {
-	char	*res;
+	char	*env;
 
 	if (name == NULL)
 		return (ft_strdup(""));
 	if (ft_strcmp(name, "?") == 0)
 		return (ft_itoa(g_exit_code));
-	res = getenv(name);
+	env = getenv(name);
 	if (res == NULL)
 		return (ft_strdup(""));
-	return (ft_strdup(res));
+	return (ft_strdup(env));
 }
