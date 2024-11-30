67e6d0cb6d795ef67e596c3975693bc68355b566
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-12-01 00:39:19 +0100

    input_validation.h: validate_input() -> ft_check_unsupported()

--- include/input_validation.h
+++ include/input_validation.h
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 11:35:47 by root              #+#    #+#             */
-/*   Updated: 2024/12/01 00:13:22 by asagymba         ###   ########.fr       */
+/*   Updated: 2024/12/01 00:31:16 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,26 @@
  * Constants.
  * ---------------------------------------------------------------------------
  */
-# define ERR_UNSUPPORTED "Unsupported characters (||, &, &&, ; or \\)\n"
+# define ERR_UNSUPPORTED "||, &, &&, ; and \\ are unsupported.\n"
 
 /**
  * ---------------------------------------------------------------------------
  * Functions.
  * ---------------------------------------------------------------------------
  */
-int	validate_input(const char *str);
+/**
+ * Checks if $str contains any of the unsupported characters:
+ * 	||, &, &&, ; and \.
+ * If it does, then error message is also generated to stderr by using
+ * ft_errmsg().
+ * @brief	Check for unsupported characters and generate error message
+ * 			if any is found.
+ * @param	str	The string to check.
+ * @return	(Some positive value), if no unsupported characters are found;
+ * 			(0), if unsupported characters and error message was successfully
+ * 				written to stderr;
+ * 			(-1), if write to stderr has failed.
+ */
+int	ft_check_unsupported(const char *str);
 
 #endif /* INPUT_VALIDATION_H */
