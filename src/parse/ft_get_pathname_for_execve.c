13c7d7323e4526326169bc3075b9c61b0ef13c7b
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-12-01 00:56:57 +0100

    ft_get_pathname: More real code, adjust Makefile

--- src/parse/ft_get_pathname_for_execve.c
+++ src/parse/ft_get_pathname_for_execve.c
@@ -6,22 +6,27 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:49:21 by asagymba          #+#    #+#             */
-/*   Updated: 2024/11/30 01:32:56 by asagymba         ###   ########.fr       */
+/*   Updated: 2024/12/01 00:55:35 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <parse.h>
-#include <stddef.h>
 #include <libft.h>
+#include <stddef.h>
 #include <stdlib.h>
 
 t_ret	ft_get_pathname_for_execve(const char *arg)
 {
 	t_ret	ret;
-	char	**path_env_split;
 
-	ret.ret = NULL;
-	if (*arg == '/' || *arg == '.')
+	if (ft_is_builtin(arg))
+	{
+		ret.ret = ft_strdup(arg);
+		if (ret.ret == NULL)
+			return ((t_ret){(-1), NULL});
+		return (ret.status = PATHNAME_IS_BUILTIN, ret);
+	}
+	else if (*arg == '/' || *arg == '.')
 	{
 		ret.status = ft_check_pathname(arg);
 		if (ret.status == PATHNAME_OK)
@@ -32,11 +37,5 @@ t_ret	ft_get_pathname_for_execve(const char *arg)
 		}
 		return (ret);
 	}
-	path_env_split = ft_split(getenv("PATH"), ':');
-	if (path_env_split == NULL)
-		return ((t_ret){(-1), NULL});
-	ret = ft_get_pathname_for_execve_from_path_env(arg,
-			(const char **)path_env_split);
-	ft_split_free(path_env_split);
-	return (ret);
+	return (ft_get_pathname_for_execve_from_path_env(arg));
 }
