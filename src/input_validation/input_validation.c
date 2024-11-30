3bb3fa68f1f334ce1d0a1ad8c5161900d110d6a5
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-12-01 00:17:18 +0100

    input_validation.c: Refactor

--- src/input_validation/input_validation.c
+++ src/input_validation/input_validation.c
@@ -6,61 +6,13 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 10:50:14 by root              #+#    #+#             */
-/*   Updated: 2024/11/28 11:55:27 by root             ###   ########.fr       */
+/*   Updated: 2024/12/01 00:16:04 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <input_validation.h>
 #include <parse.h>
 #include <unistd.h>
-
-#define ERR_QUOTE "Error: Unclosed quotes\n"
-#define ERR_PIPE_START "bash: syntax error near unexpected token `|'\n"
-#define ERR_PIPE_END "Error: Unexpected end of input\n"
-#define ERR_UNSUPPORTED "Error: Unsupported characters (||, &&, ;, \\)\n"
-#define ERR_EMPTY_INPUT "Error: Input contains only spaces\n"
-
-/**
- * @brief Checks if the string contains only whitespace characters.
- *
- * @param str
- * @return (int 1 if the string contains only whitespace characters);
- * 			0 otherwise.
- */
-int	input_issspace(const char *str)
-{
-	while (*str != '\0' && ft_isspace(*str))
-		str++;
-	if (*str == '\0')
-	{
-		(void)write(STDERR_FILENO, ERR_EMPTY_INPUT, sizeof(ERR_EMPTY_INPUT));
-		return (-1);
-	}
-	else
-		return (0);
-}
-
-static int	has_invalid_pipe_position(const char *str)
-{
-	const char	*end;
-
-	while (*str != '\0' && ft_isspace(*str))
-		str++;
-	if (*str == '|')
-	{
-		(void)write(STDERR_FILENO, ERR_PIPE_START, sizeof(ERR_PIPE_START) - 1);
-		return (-1);
-	}
-	end = str + ft_strlen(str) - 1;
-	while (end > str && ft_isspace(*end))
-		end--;
-	if (*end == '|')
-	{
-		(void)write(STDERR_FILENO, ERR_PIPE_END, sizeof(ERR_PIPE_END) - 1);
-		return (-1);
-	}
-	return (0);
-}
 
 static int	contains_unsupported_characters(const char *str)
 {
