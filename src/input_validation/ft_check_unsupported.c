e84c3aa4d14eb7cea167ff8acc46656859842ffe
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-12-01 00:38:04 +0100

    input_validation.c -> ft_check_unsupported.c

---
+++ src/input_validation/ft_check_unsupported.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_unsupported.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 10:50:14 by root              #+#    #+#             */
+/*   Updated: 2024/12/01 00:28:56 by asagymba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <input_validation.h>
+#include <parse.h>
+#include <libft.h>
+#include <utils.h>
+
+int	ft_check_unsupported(const char *str)
+{
+	enum e_quotes_type	quotes_type;
+
+	quotes_type = no_quotes;
+	while (*str != '\0')
+	{
+		if (*str == '\'' || *str == '\"')
+			ft_handle_quotes(*str, &quotes_type);
+		if (quotes_type == no_quotes)
+		{
+			if (*str == '&' || *str == ';' || *str == '\\'
+				|| ft_strncmp(str, "||", sizeof("||") - 1) == 0
+				|| ft_strncmp(str, "&&", sizeof("&&") - 1) == 0)
+			{
+				if (ft_errmsg(ERR_UNSUPPORTED) == -1)
+					return (-1);
+				return (0);
+			}
+		}
+		str++;
+	}
+	return (1);
+}
