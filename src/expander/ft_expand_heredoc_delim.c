8416ab41af845f9814495648ab864b7320630849
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-12-04 19:01:26 +0100

    Fix ft_expand_heredoc_delim()

--- src/expander/ft_expand_heredoc_delim.c
+++ src/expander/ft_expand_heredoc_delim.c
@@ -6,23 +6,34 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 18:47:01 by asagymba          #+#    #+#             */
-/*   Updated: 2024/12/04 18:50:56 by asagymba         ###   ########.fr       */
+/*   Updated: 2024/12/04 19:01:08 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <expander.h>
+#include <parse.h>
 #include <libft.h>
 
 void	ft_expand_heredoc_delim(char *heredoc_delim)
 {
+	enum e_quotes_type	quotes_type;
+	enum e_quotes_type	new_quotes_type;
+
+	quotes_type = no_quotes;
 	while (*heredoc_delim != '\0')
 	{
 		if (*heredoc_delim == '\'' || *heredoc_delim == '\"')
 		{
-			(void)ft_memmove(heredoc_delim, heredoc_delim + 1,
-				ft_strlen(heredoc_delim));
-			continue ;
+			new_quotes_type = quotes_type;
+			ft_handle_quotes(*heredoc_delim, &new_quotes_type);
+			if (new_quotes_type != quotes_type)
+				(void)ft_memmove(heredoc_delim, heredoc_delim + 1,
+					ft_strlen(heredoc_delim));
+			else
+				heredoc_delim++;
+			quotes_type = new_quotes_type;
 		}
-		heredoc_delim++;
+		else
+			heredoc_delim++;
 	}
 }
