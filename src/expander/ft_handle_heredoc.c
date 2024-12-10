9f6f5c58673b2392d00ba98c6c506be14e94beda
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-12-10 13:31:21 +0100

    ft_handle_heredoc: Don't write any errlogs related to write()

--- src/expander/ft_handle_heredoc.c
+++ src/expander/ft_handle_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 12:51:23 by asagymba          #+#    #+#             */
-/*   Updated: 2024/12/10 13:05:32 by asagymba         ###   ########.fr       */
+/*   Updated: 2024/12/10 13:30:37 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,7 @@ int	ft_handle_heredoc(const char *eof_str)
 		input = readline("heredoc> ");
 		if (input == NULL)
 		{
-			if (close(ret[1]) == -1)
-				return ((void)close(ret[0]),
-					(void)ft_errmsg("close()-san fa1led ;("), -1);
-			else if (ft_errmsg("heredoc: caught EOF") == -1)
+			if (close(ret[1]) == -1 || ft_errmsg("heredoc: caught EOF") == -1)
 				return ((void)close(ret[0]), -1);
 			return (ret[0]);
 		}
@@ -41,8 +38,7 @@ int	ft_handle_heredoc(const char *eof_str)
 			return (free(input), ret[0]);
 		if (write(ret[1], input, ft_strlen(input)) == -1
 			|| write(ret[1], "\n", 1) == -1)
-			return ((void)close(ret[0]), (void)close(ret[1]),
-				(void)ft_errmsg("write()-san fa1led x_x"), free(input), -1);
+			return ((void)close(ret[0]), (void)close(ret[1]), free(input), -1);
 		free(input);
 	}
 }
