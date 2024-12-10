ab70f04d49f8b136152b70e1714dbd1129415a6b
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-12-10 15:08:52 +0100

    ft_get_next_token(): Keep track of currently opened quotes

--- src/parse/ft_get_next_token.c
+++ src/parse/ft_get_next_token.c
@@ -6,7 +6,7 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 12:00:11 by asagymba          #+#    #+#             */
-/*   Updated: 2024/12/03 19:46:58 by asagymba         ###   ########.fr       */
+/*   Updated: 2024/12/10 15:08:10 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@
 
 char	*ft_get_next_token(char *prompt, char **saveptr)
 {
-	char	*ret;
+	char				*ret;
+	enum e_quotes_type	quotes;
 
 	if (prompt != NULL)
 		*saveptr = prompt;
 	ret = *saveptr;
 	if (*ret == '\0')
 		return (NULL);
+	quotes = no_quotes;
 	while (**saveptr != '\0')
 	{
-		if (**saveptr == '|')
+		if (**saveptr == '\'' || **saveptr == '\"')
+			ft_handle_quotes(**saveptr, &quotes);
+		else if (**saveptr == '|' && quotes == no_quotes)
 		{
 			*(*saveptr)++ = '\0';
 			break ;
