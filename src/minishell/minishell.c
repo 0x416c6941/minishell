55fa239ad77aee863c79d7d480ee7dbfb4b0fd2e
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-12-15 23:58:19 +0100

    update TODO

--- src/minishell/minishell.c
+++ src/minishell/minishell.c
@@ -6,13 +6,63 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 20:33:48 by asagymba          #+#    #+#             */
-/*   Updated: 2024/12/15 00:17:46 by asagymba         ###   ########.fr       */
+/*   Updated: 2024/12/15 23:28:48 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 #include <stdbool.h>
 #include <parse.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
+
+void	signal_reset_prompt(int signo)
+{
+	(void)signo;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	ignore_sigquit(void)
+{
+	struct sigaction	act;
+
+	ft_memset(&act, 0, sizeof(act));
+	act.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &act, NULL);
+}
+
+void	set_signals_interactive(void)
+{
+	struct sigaction	act;
+
+	ignore_sigquit();
+	ft_memset(&act, 0, sizeof(act));
+	act.sa_handler = &signal_reset_prompt;
+	sigaction(SIGINT, &act, NULL);
+}
+
+void	signal_print_newline(int signal)
+{
+	(void)signal;
+	rl_on_new_line();
+}
+
+void	set_signals_noninteractive(void)
+{
+	struct sigaction	act;
+
+	ft_memset(&act, 0, sizeof(act));
+	act.sa_handler = &signal_print_newline;
+	sigaction(SIGINT, &act, NULL);
+	sigaction(SIGQUIT, &act, NULL);
+}
 
 int	minishell(t_minishell_data *data)
 {
@@ -21,7 +71,7 @@ int	minishell(t_minishell_data *data)
 	while (42)
 	{
 		if (data->is_interactive)
-			(void)data; // SET INTERACTIVE MODE HERE
+			set_signals_interactive();
 		status = ft_get_execs(data);
 		if (status == -1)
 			return (data->should_leave = true,
@@ -31,7 +81,7 @@ int	minishell(t_minishell_data *data)
 		else if (status == MINISHELL_INPUT_EOF)
 			return (data->should_leave = true,
 				data->with_which_code = 0, 0);
-		// SET NON-INTERACTIVE MODE HERE
+		set_signals_noninteractive();
 		ft_check_syntax_and_execute(data);
 		if (data->should_leave)
 		{
