c37f720e793da8755515c737cd0e57f8a4b05faf
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-12-15 14:28:22 +0100

    env_builtin: Update to not write variables without value

--- src/builtins/env_builtin.c
+++ src/builtins/env_builtin.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 14:16:19 by root              #+#    #+#             */
-/*   Updated: 2024/12/15 13:44:34 by asagymba         ###   ########.fr       */
+/*   Updated: 2024/12/15 14:27:59 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,23 @@
 #include <unistd.h>
 #include <libft.h>
 #include <stdbool.h>
+
+/**
+ * Prints the variable in format "key=value".
+ * @brief	Norminette bypass.
+ * @param	env	Environment to print to stdout.
+ * @return	(-1), if some write() failed;
+ * 			(Some non-negative value) otherwise.
+ */
+static int	ft_env_print_helper(const t_env *env)
+{
+	if (write(STDOUT_FILENO, env->key, ft_strlen(env->key)) == -1
+		|| write(STDOUT_FILENO, "=", 1) == -1
+		|| write(STDOUT_FILENO, env->value, ft_strlen(env->value)) == -1
+		|| write(STDOUT_FILENO, "\n", 1) == -1)
+		return (-1);
+	return (0);
+}
 
 int	env_builtin(t_minishell_data *data, const char *args[])
 {
@@ -36,12 +53,12 @@ int	env_builtin(t_minishell_data *data, const char *args[])
 	while (node != NULL)
 	{
 		env = (t_env *)node->content;
-		if (write(STDOUT_FILENO, env->key, ft_strlen(env->key)) == -1
-			|| write(STDOUT_FILENO, "=", 1) == -1
-			|| write(STDOUT_FILENO, env->value, ft_strlen(env->value)) == -1
-			|| write(STDOUT_FILENO, "\n", 1) == -1)
-			return (data->should_leave = true,
-				data->with_which_code = MESSED_UP, EXIT_ERROR);
+		if (env->value != NULL)
+		{
+			if (ft_env_print_helper(env) == -1)
+				return (data->should_leave = true,
+					data->with_which_code = MESSED_UP, EXIT_ERROR);
+		}
 		node = node->next;
 	}
 	return (EXIT_OK);
--- src/builtins/export_builtin.c
+++ src/builtins/export_builtin.c
@@ -6,97 +6,18 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 16:29:58 by root              #+#    #+#             */
-/*   Updated: 2024/12/14 10:32:56 by root             ###   ########.fr       */
+/*   Updated: 2024/12/15 14:08:01 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <builtins.h>
+#include <minishell.h>
 
-#define EXPORT_PREFIX "minishell: export: '"
-#define EXPORT_ERROR "' not a valid identifier\n"
-
-static void	export_err_msg(const char *msg)
+int	export_builtin(t_minishell_data *data, const char *args[])
 {
-	if (write(STDERR_FILENO, EXPORT_PREFIX, ft_strlen(EXPORT_PREFIX)) == -1)
-		exit(EXIT_FATAL_ERROR);
-	if (write(STDERR_FILENO, msg, ft_strlen(msg)) == -1)
-		exit(EXIT_FATAL_ERROR);
-	if (write(STDERR_FILENO, EXPORT_ERROR, ft_strlen(EXPORT_ERROR)) == -1)
-		exit(EXIT_FATAL_ERROR);
-}
-
-static int	write_env_variable(const char *prefix, t_env *env)
-{
-	if (write(STDOUT_FILENO, prefix, ft_strlen(prefix)) == -1)
-		return (EXIT_FATAL_ERROR);
-	if (write(STDOUT_FILENO, env->key, ft_strlen(env->key)) == -1)
-		return (EXIT_FATAL_ERROR);
-	if (env->value)
-	{
-		if (write(STDOUT_FILENO, "=\"", 2) == -1)
-			return (EXIT_FATAL_ERROR);
-		if (write(STDOUT_FILENO, env->value, ft_strlen(env->value)) == -1)
-			return (EXIT_FATAL_ERROR);
-		if (write(STDOUT_FILENO, "\"", 1) == -1)
-			return (EXIT_FATAL_ERROR);
-	}
-	if (write(STDOUT_FILENO, "\n", 1) == -1)
-		return (EXIT_FATAL_ERROR);
-	return (EXIT_OK);
-}
-
-static int	export_no_args_builtin(const t_list *envs)
-{
-	const char		*prefix;
-	const t_list	*current;
-	t_env			*env;
-
-	if (!envs)
-		return (EXIT_OK);
-	prefix = "declare -x ";
-	current = envs;
-	while (current)
-	{
-		env = (t_env *)current->content;
-		if (!env || !env->key)
-			return (EXIT_FATAL_ERROR);
-		if (write_env_variable(prefix, env) == EXIT_FATAL_ERROR)
-			return (EXIT_FATAL_ERROR);
-		current = current->next;
-	}
-	return (EXIT_OK);
-}
-
-static int	add_env(t_list **env_head, const char *arg)
-{
-	t_ret		ft_initialize_one_env_status;
-	t_list		*env_node;
-	t_env		*new_env;
-	const char	*unset_args[2];
-
-	ft_initialize_one_env_status = ft_initialize_one_env(arg);
-	if (ft_initialize_one_env_status.status == (-1))
-		return (EXIT_FATAL_ERROR);
-	env_node = ft_lstnew(ft_initialize_one_env_status.ret);
-	if (env_node == NULL)
-		return (EXIT_FATAL_ERROR);
-	new_env = (t_env *)env_node->content;
-	unset_args[0] = new_env->key;
-	unset_args[1] = NULL;
-	unset_builtin(env_head, unset_args);
-	ft_lstadd_back(env_head, env_node);
-	return (EXIT_OK);
-}
-
-int	export_builtin(t_list **env_head, const char **args)
-{
-	size_t	i;
+	t_env	*saved;
 
 	i = 0;
-	if (!env_head || !args)
-		return (EXIT_FATAL_ERROR);
-	if (!args[0])
-		return (export_no_args_builtin(*env_head));
 	while (args[i] != NULL)
 	{
 		if (!is_valid_export_arg(args[i]))
