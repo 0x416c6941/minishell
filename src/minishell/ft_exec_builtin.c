96dd55be49b6d2f2adffdb3ca49bcd7f50cea786
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2024-12-16 15:01:52 +0100

    ft_exec_builtin.c: Fix Norme

--- src/minishell/ft_exec_builtin.c
+++ src/minishell/ft_exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 01:20:53 by asagymba          #+#    #+#             */
-/*   Updated: 2024/12/16 14:30:21 by root             ###   ########.fr       */
+/*   Updated: 2024/12/16 15:01:44 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	ft_exec_builtin(t_minishell_data *data, t_exec *cmd)
 		return (pwd_builtin(data, (const char **)(cmd->args_for_execve + 1)));
 	else if (ft_strcmp(cmd->args_for_execve[0], "export") == 0)
 		return (export_builtin(data, (const char **)(cmd->args_for_execve
-					+ 1)));
+				+ 1)));
 	else if (ft_strcmp(cmd->args_for_execve[0], "unset") == 0)
 		return (unset_builtin(data, (const char **)(cmd->args_for_execve + 1)));
 	else if (ft_strcmp(cmd->args_for_execve[0], "env") == 0)
@@ -34,7 +34,7 @@ int	ft_exec_builtin(t_minishell_data *data, t_exec *cmd)
 	else if (ft_strcmp(cmd->args_for_execve[0], "exit") == 0)
 	{
 		if (exit_builtin(data, (const char **)(cmd->args_for_execve
-					+ 1)) == EXIT_OK)
+				+ 1)) == EXIT_OK)
 			return (data->with_which_code);
 		else
 			return (EXIT_ERROR);
