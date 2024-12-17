25e30dfcb6072401aa88a34a6424358ac11848e0
hlyshchu <gleb.lischuk@gmail.com>
2024-12-17 08:58:07 +0100

    small fix in signal.c. perror function is deleted, we don't use them

--- src/signals/signal.c
+++ src/signals/signal.c
@@ -6,16 +6,16 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 13:57:25 by root              #+#    #+#             */
-/*   Updated: 2024/12/16 15:01:05 by asagymba         ###   ########.fr       */
+/*   Updated: 2024/12/17 08:57:17 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <minishell.h>
-#include <stdio.h>
 #include <readline/history.h>
 #include <readline/readline.h>
 #include <signal.h>
+#include <stdio.h>
 #include <unistd.h>
 
 static void	sig_reset_prompt(int signo)
@@ -48,8 +48,8 @@ int	handle_signal_noninteractive(void)
 
 	ft_memset(&act, 0, sizeof(act));
 	act.sa_handler = &sig_print_newline;
-	if (sigaction(SIGINT, &act, NULL) == -1
-		|| sigaction(SIGQUIT, &act, NULL) == -1)
+	if (sigaction(SIGINT, &act, NULL) == -1 || sigaction(SIGQUIT, &act, NULL) ==
+		-1)
 		return (-1);
 	return (0);
 }
@@ -62,9 +62,6 @@ int	handle_signal_interactive(void)
 	ft_memset(&act, 0, sizeof(act));
 	act.sa_handler = &sig_reset_prompt;
 	if (sigaction(SIGINT, &act, NULL) == -1)
-	{
-		perror("sigaction SIGINT failed");
 		return (-1);
-	}
 	return (0);
 }
